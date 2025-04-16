ff27c93a33c9214f6558bbb62a1de8f518703b3c
ImanolSayago <imanolsayago0@gmail.com>
2025-04-15 22:05:24 -0300

    Arrancando Front y logica de negocio

---
+++ src/app/ComponentesPrincipales/nav/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  isDropdownOpen = false;
+  isMenuOpen = false;
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  
+}
